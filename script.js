ab7435d90240a0a424370121a8a7a89be3ba26ed
DhiasCoder <diashabibi8@gmail.com>
2025-01-04 18:25:45 +0700

    Second 4

---
+++ script.js
@@ -0,0 +1,65 @@
+// Toggle navigation and burger animation
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    burger.classList.toggle("toggle");
+  });
+});
+
+// Clear form before unload
+window.onbeforeunload = () => {
+  document.querySelectorAll("form").forEach((form) => form.reset());
+};
+
+// JavaScript untuk navigasi antar halaman
+const links = document.querySelectorAll("nav a");
+const pages = document.querySelectorAll(".page");
+
+links.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const pageId = link.getAttribute("data-page");
+
+    // Sembunyikan semua halaman
+    pages.forEach((page) => page.classList.remove("active"));
+
+    // Tampilkan halaman yang diklik
+    document.getElementById(pageId).classList.add("active");
+  });
+});
+
+// Ambil elemen tombol
+const btn = document.querySelectorAll("section .btn");
+const page = document.querySelectorAll(".page");
+
+// Tambahkan event listener untuk menangani klik
+btn.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const pageId = link.getAttribute("data-page");
+
+    // Sembunyikan semua halaman
+    page.forEach((page) => page.classList.remove("active"));
+
+    // Tampilkan halaman yang diklik
+    document.getElementById(pageId).classList.add("active");
+  });
+});
+
+window.addEventListener("load", function () {
+  window.scrollTo(0, 0);
+});
+
+// Loading Spinner
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+
+  loader.classList.add("loader--hidden");
+
+  loader.addEventListener("transitionend", () => {
+    document.body.removeChild(loader);
+  });
+});
